5a2f04260508af11866883fbca443243e05c31d2
Rapha <raphanicaise@gmail.com>
2024-07-01 19:49:45 -0300

    Cambio en decimal

--- back-end/SQLScripts/Functions.sql
+++ back-end/SQLScripts/Functions.sql
@@ -2,7 +2,7 @@ delimiter //
 CREATE FUNCTION calcular_calificacion(claridad_profesor_calif INT, precio_profesor_calif INT, disponibilidad_profesor_calif INT)
 RETURNS DECIMAL(10,2)
 BEGIN
-    DECLARE calificacion DECIMAL(10,2);
+    DECLARE calificacion DECIMAL(3,2);
     SET calificacion = (claridad_profesor_calif + precio_profesor_calif + disponibilidad_profesor_calif) / 3;
     RETURN calificacion;
 END // 
